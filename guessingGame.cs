a7282bc929a422bb5f53280ad04f9b5299255405
c4llmeco4ch <cbb692@gmail.com>
2019-05-14 02:51:02 -0700

    Added replay loop

--- guessingGame.cs
+++ guessingGame.cs
@@ -1,29 +1,37 @@
 using System;
 public class GuessingGame{
     public static void Main(string[] args){
-        int high = 100;
-        int low = 0;
-        int guess = 50;
-        bool isCorrect = false;
-        while(!isCorrect){
-            Console.WriteLine("My guess is {0}", guess);
-            Console.WriteLine("Is my guess correct (c), too high (h), or too low (l)? ");
-            switch(Console.ReadLine()){
-                case "c":
-                    isCorrect = !isCorrect;
-                    Console.WriteLine("Yay!");
-                    break;
-                case "h":
-                    high = guess - 1;
-                    guess = Calculate(low, high);
-                    break;
-                case "l":
-                    low = guess + 1;
-                    guess = Calculate(low, high);
-                    break;
-                default:
-                    Console.WriteLine("Invalid Input, please use \"c\", \"h\", or \"l\".");
-                    break;
+        bool arePlaying = true;
+        while (arePlaying){
+            int high = 100;
+            int low = 0;
+            int guess = 50;
+            bool isCorrect = false;
+            while(!isCorrect){
+                Console.WriteLine("My guess is {0}", guess);
+                Console.WriteLine("Is my guess correct (c), too high (h), or too low (l)? ");
+                switch(Console.ReadLine()){
+                    case "c":
+                        isCorrect = !isCorrect;
+                        Console.WriteLine("Yay!");
+                        break;
+                    case "h":
+                        high = guess - 1;
+                        guess = Calculate(low, high);
+                        break;
+                    case "l":
+                        low = guess + 1;
+                        guess = Calculate(low, high);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Input, please use \"c\", \"h\", or \"l\".");
+                        break;
+                }
+            }
+            Console.WriteLine("Want to play again (y/n)? ");
+            string ans = Console.ReadLine();
+            if(!ans.Equals("y") && !ans.Equals("yes")){
+                arePlaying = false;
             }
         }
     }
